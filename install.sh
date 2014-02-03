fc9936c3742784bd49342e4601d3556520cc31f1
Benjamin Chodoroff <ben@theworkdept.com>
2014-02-02 20:47:09 -0500

    ruby and bundler

--- install.sh
+++ install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+
+# ruby and bundler
+curl -L get.rvm.io | bash -s stable
+bash -c "source /etc/profile.d/rvm.sh && rvm requirements; rvm install 1.9.1; gem install bundler"
+
+# drupal
 cd /var/www
 drush make --working-copy https://raw.github.com/detroitledger/gnl_profile/7.x-1.x/distro.make drupal
 cd drupal
 mysql -uroot -ppuppetdrupal -e 'create database gnl_test'
 drush site-install gnl_profile  --account-name=admin --account-pass=admin --db-url=mysql://root:puppetdrupal@localhost/gnl_test --yes
-(cd profiles/gnl_profile/themes/gnl_theme; compass compile)
+(cd profiles/gnl_profile/themes/gnl_theme; bundle install; compass compile)
 drush cc all
 bash ./profiles/gnl_profile/modules/custom/migrate_gnl/download.sh # requires write access to /tmp
 drush mreg
