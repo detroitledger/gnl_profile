a7e7911c45d6ab123519031d9dc37d8f5b2ad66a
Benjamin Chodoroff <bc@thermitic.net>
2014-02-06 15:11:07 -0500

    use docker var for mysql host

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ cd /var/www
 drush make --working-copy https://raw.github.com/detroitledger/gnl_profile/7.x-1.x/distro.make drupal
 cd drupal
 mysql -uroot -ppuppetdrupal -e 'create database gnl_test'
-drush site-install gnl_profile  --account-name=admin --account-pass=admin --db-url=mysql://root:puppetdrupal@localhost/gnl_test --yes
+drush site-install gnl_profile  --account-name=admin --account-pass=admin --db-url=mysql://root:puppetdrupal@$DB_PORT_3306_TCP_ADDR/gnl_test --yes
 (cd profiles/gnl_profile/themes/gnl_theme; bundle install; compass compile)
 drush cc all
 bash ./profiles/gnl_profile/modules/custom/migrate_gnl/download.sh # requires write access to /tmp
