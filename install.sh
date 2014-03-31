b8d90300da082c8e8ee07be903ebf6af4bdc54ce
Benjamin Chodoroff <ben@theworkdept.com>
2014-03-30 20:32:35 -0400

    oops wrong ruby version

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # ruby and bundler
 curl -L get.rvm.io | bash -s stable
-bash -c "source /etc/profile.d/rvm.sh && rvm requirements; rvm install 1.9.1; gem install bundler"
+bash -c "source /etc/profile.d/rvm.sh && rvm requirements; rvm install ruby-1.9.3-p484; gem install bundler"
 
 # drupal
 cd /var/www
