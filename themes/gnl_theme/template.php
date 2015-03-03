e8329c04ca28ec37c811067fe134d24b096661e8
Benjamin Chodoroff <ben@theworkdept.com>
2015-03-03 14:26:41 -0500

    management links in view block

--- themes/gnl_theme/template.php
+++ themes/gnl_theme/template.php
@@ -12,3 +12,26 @@ function gnl_theme_breadcrumb($vars) {
     return;
   }
 }
+
+/**
+ * Implements template_preprocess().
+ */
+function gnl_theme_preprocess(&$variables, $hook) {
+  if ($hook == 'views_more') {
+    $view = $variables['view'];
+    if ($view->name == 'board_terms' && $view->current_display == 'block_1' && count($view->args)) {
+      $node = node_load($view->args[0]);
+
+      if (gnl_schema_show_management_links($node, 'full')) {
+        $links = gnl_schema_management_links($node);
+        $variables['management_links'] = theme_links(array(
+          'links' => $links,
+          'attributes' => array(
+            'class' => 'gnl-management-links',
+          ),
+        ));
+      }
+    }
+  }
+}
+
