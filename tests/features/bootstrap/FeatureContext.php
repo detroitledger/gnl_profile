df7e7398150b61a678b157f68f33dc97bfb9d130
Benjamin Chodoroff <ben@theworkdept.com>
2015-03-04 16:43:24 -0500

    use our own loggedIn function

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -20,4 +20,68 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
   public function __construct() {
   }
 
+  /**
+   * Determine if the a user is already logged in,
+   * checking /user page and body class 'logged-in'.
+   *
+   * @return boolean
+   *   Returns TRUE if a user is logged in for this session.
+   */
+  public function loggedIn() {
+    $session = $this->getSession();
+    $session->visit($this->locatePath('/user'));
+    $page = $session->getPage();
+    $logged_in = $page->find('css', '.logged-in');
+
+    // body element should have class logged-in
+    return ($logged_in !== null);
+  }
+
+  // Re-implement any function that needs to use our loggedIn():
+  /**
+   * User with a given role is already logged in.
+   *
+   * @param string $role
+   *   A single role, or multiple comma-separated roles in a single string.
+   *
+   * @return boolean
+   *   Returns TRUE if the current logged in user has this role (or roles).
+   */
+  public function loggedInWithRole($role) {
+    return $this->loggedIn() && $this->user && isset($this->user->role) && $this->user->role == $role;
+  }
+
+  /**
+   * Creates and authenticates a user with the given role(s).
+   *
+   * @Given I am logged in as an editor
+   */
+  public function assertAuthenticatedAsEditor() {
+    $role = 'editor';
+    // Check if a user with this role is already logged in.
+    if (!$this->loggedInWithRole($role)) {
+      // Create user (and project)
+      $user = (object) array(
+        'name' => $this->getRandom()->name(8),
+        'pass' => $this->getRandom()->name(16),
+        'role' => $role,
+      );
+      $user->mail = "{$user->name}@example.com";
+
+      $this->userCreate($user);
+
+      $roles = explode(',', $role);
+      $roles = array_map('trim', $roles);
+      foreach ($roles as $role) {
+        if (!in_array(strtolower($role), array('authenticated', 'authenticated user'))) {
+          // Only add roles other than 'authenticated user'.
+          $this->getDriver()->userAddRole($user, $role);
+        }
+      }
+
+      // Login.
+      $this->login();
+    }
+  }
+
 }
