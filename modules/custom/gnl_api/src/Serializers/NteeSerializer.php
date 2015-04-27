bad144ced08180a0a39597a70ce41cc7aecfd552
Benjamin Chodoroff <ben@theworkdept.com>
2015-04-26 22:55:36 -0400

    offer ntees endpoint & optionally load related organizations

--- modules/custom/gnl_api/src/Serializers/NteeSerializer.php
+++ modules/custom/gnl_api/src/Serializers/NteeSerializer.php
@@ -10,6 +10,7 @@
 namespace Drupal\gnl_api\Serializers;
 
 use Tobscure\JsonApi\SerializerAbstract;
+use Drupal\gnl_api\Serializers\OrgSerializer;
 
 class NteeSerializer extends SerializerAbstract {
   protected $type = 'ntees';
@@ -25,6 +26,10 @@ class NteeSerializer extends SerializerAbstract {
   protected function orgs() {
     return function ($ntee, $include, $included) {
       $serializer = new OrgSerializer($included);
-    }
+      $orgs = $serializer->collection($include ? $ntee->orgs : $post->org_ids);
+      $link = new Link($orgs);
+      return $link;
+    };
   }
 }
+
